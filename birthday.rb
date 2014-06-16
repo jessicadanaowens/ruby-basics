6e06192ffd4dc2bbc81dc9c6752a4d904bfb37e5
Jessica Owens <jessicadanaowens@gmail.com>
2014-06-16 17:26:58 -0600

    birthday.rb

--- birthday.rb
+++ birthday.rb
@@ -1,7 +1,26 @@
+require 'date'
+
 puts "What is your name?"
 
-name = gets.chomp
+name = gets.chomp.capitalize
 
 filename = 'birthday_data.csv'
 
-File.open filename 
+txt = File.read filename
+
+birthday_info = txt.split(',')
+
+i = 0
+
+for each in birthday_info
+  if name == each
+    b = birthday_info[i+1]
+    year = b[0..3]
+    age = Time.new.year - year.to_i
+    puts name + " " + b + " " + age.to_s
+  end
+
+  i += 1
+
+end
+
