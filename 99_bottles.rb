bfd2d1be111939b0d5980de68fdeb2681d17f2f3
Jessica Owens <jessicadanaowens@gmail.com>
2014-06-17 15:10:24 -0600

    improved 99_bottles.rb

--- 99_bottles.rb
+++ 99_bottles.rb
@@ -1,11 +1,3 @@
-#Write a program that prints out verses of "99 bottles of beer on the wall",
-# according to the following rules: * The user must type "sing 88", where 88
-# is any positive number * If the user runs the program without typing anything,
-# print "I need to know how many bottles to sing!" * If the user runs the
-# program and specifies a verse with "sing <number>", print all the lines from
-# the song starting at that verse * If the user types anything but "sing <number>",
-# show an error and exit
-
 puts "Let's sing 99 bottles of beer on the wall!"
 puts "Where should we start? Please type 'sing' + "
 puts "the number of bottles of beer on the wall"
@@ -16,29 +8,19 @@ response = gets.chomp
 
 beer = (response.split(' '))[1].to_i
 
-puts beer
+(2..beer).to_a.reverse.each do |num|
 
-def sing_song(beer)
+  puts num.to_s + " bottles of beers on the wall!!" + num.to_s
+  puts " bottles of beer!!"
+  puts "Take one down and pass it around"
 
-  while beer >= 1
-    puts beer.to_s + " bottles of beers on the wall!!" + beer.to_s
-    puts " bottles of beer!!"
-    puts "Take one down and pass it around"
 
-    beer -= 1
-    puts beer.to_s + "bottles of beer on the wall!!"
-  end
-
-  if beer >= 1
-  sing_song(beer)
-  else
-    puts "No bottles of beer on the wall!!"
-    puts "No bottles of beer!!"
-    puts "I don't feel so well...blah!!"
-  end
+  puts (num - 1).to_s + "bottles of beer on the wall!!"
 
 end
 
-sing_song(beer)
+puts "1 bottle of beer ont the wall, one bottle of heer."
+puts "take one down and pass it around"
+puts "no bottles of beer on the wall."
 
 
