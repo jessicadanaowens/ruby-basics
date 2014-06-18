3042d6a9442ed81776ed74d618bdd6ba25082236
Jessica Owens <jessicadanaowens@gmail.com>
2014-06-17 20:04:56 -0600

    solo deafgrandma

--- deafgrandma.rb
+++ deafgrandma.rb
@@ -1,18 +1,13 @@
-
-def get_response
-  puts "Please write something, or 'exit' to exit the program"
+def deaf_grandma
+  puts "say something to grandma"
   response = gets.chomp
-  if response == "exit"
-    exit
-  elsif response != response.upcase
+  while response != response.upcase
     puts "HUH?! SPEAK UP SONNY!"
-    get_response
-  else
-    puts "NO, NOT SINCE #{rand(1930..1950)}!"
-    get_response
+    response = gets.chomp
   end
+
+  puts "NO, NOT SINCE #{rand(1930..1950)}!"
+
 end
-
-get_response
-
+deaf_grandma
 
